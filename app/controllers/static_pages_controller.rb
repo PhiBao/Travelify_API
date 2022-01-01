88c8c9ea2632501edcf615db88ac62663b207496
PhiBao <kiter0211@gmail.com>
2022-01-01 12:16:07 +0700

    Change amount of featured home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,17 @@
 class StaticPagesController < ApplicationController
   
   def home
-    featured = Tour.valid.first
+    featured = Tour.valid.first(3)
     hot_tours = Tour.valid.take(9)
     new_tours = Tour.valid.last(9)
-    list = ([].concat(hot_tours, new_tours).push(featured)).uniq(&:id)
+    list = ([].concat(hot_tours, new_tours, featured)).uniq(&:id)
     hot_tags = Tag.popularity.first(6)
     
     render json: { 
       data:
         { 
           list: TourBlueprint.render_as_hash(list, view: :normal),
-          featured: featured.id,
+          featured: featured.pluck(:id),
           hot_tours: hot_tours.pluck(:id),
           new_tours: new_tours.pluck(:id),
           hot_tags: TagBlueprint.render_as_hash(hot_tags, view: :home)
